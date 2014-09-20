080f0f8c59e33bf3291d37babc156d4499ad167f
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2014-09-20 21:00:33 +0100

    Updated to include user table

--- develop/example/mysql-example.sql
+++ develop/example/mysql-example.sql
@@ -139,9 +139,24 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `myapp`;
-INSERT INTO `example` (`id`, `name`, `email`, `note`) VALUES (1, 'Matus Zeman', 'matus.zeman@gmail.com', 'This seems easy!');
+INSERT INTO `example` (`id`, `name`, `email`, `note`) VALUES (1, 'Fredrik Wahlqvist', 'fwahlqvist@gmail.com', 'This seems easy!');
 
 COMMIT;
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+
+
+CREATE TABLE `user`
+(
+    `user_id`       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username`      VARCHAR(255) DEFAULT NULL UNIQUE,
+    `email`         VARCHAR(255) DEFAULT NULL UNIQUE,
+    `display_name`  VARCHAR(50) DEFAULT NULL,
+    `password`      VARCHAR(128) NOT NULL,
+    `state`         SMALLINT UNSIGNED
+) ENGINE=InnoDB CHARSET="utf8";
 
 
 -- -----------------------------------------------------
