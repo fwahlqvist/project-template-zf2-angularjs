b5756f2654d473cd327f053e55502c943ec755ca
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2014-09-23 22:30:21 +0200

    Added scn-social-auth mysql

--- develop/example/mysql-example.sql
+++ develop/example/mysql-example.sql
@@ -160,6 +160,22 @@ CREATE TABLE `user`
 
 
 -- -----------------------------------------------------
+-- Table `user_provider`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS 'user_provider' (
+  'user_id' INT NOT NULL,
+  'provider_id' varchar(50) NOT NULL,
+  'provider' varchar(255) NOT NULL,
+  PRIMARY KEY ('user_id','provider_id'),
+  UNIQUE KEY ('provider_id','provider'),
+  FOREIGN KEY ('user_id') REFERENCES 'user' ('user_id')
+) ENGINE=InnoDB;
+
+
+
+
+-- -----------------------------------------------------
 -- Data for table `oauth_clients`
 -- -----------------------------------------------------
 START TRANSACTION;
